d7852791c0a97659378e7ce811d69d7298373e14
Aitor Rodriguez Gallardo <aitor.gallardo.14@gmail.com>
2023-06-18 19:13:01 +0200

    Añadido dos endpoint para agregar entradas

--- src/main/java/com/aitor/organizadordoc/web/DtoController.java
+++ src/main/java/com/aitor/organizadordoc/web/DtoController.java
@@ -3,17 +3,23 @@ package com.aitor.organizadordoc.web;
 import com.aitor.organizadordoc.entity.AudioLibro;
 import com.aitor.organizadordoc.entity.Libro;
 import com.aitor.organizadordoc.repository.*;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
 
 @Controller
 public class DtoController {
-
+    Logger logger = LoggerFactory.getLogger(DtoController.class);
     @Autowired
     AudioLibroRepository audioLibroRepository;
     @Autowired
@@ -36,5 +42,37 @@ public class DtoController {
         objects.addAll(audioLibros);
         model.addAttribute("objetos", objects);
         return "dashboard";
+    }
+
+    @PostMapping("/api/nuevo/libro")
+    public ResponseEntity<Libro> crearLibro(@RequestBody Libro libro) {
+        Libro returnedLibro = null;
+        try {
+            returnedLibro = libroRepository.save(libro);
+        } catch (Exception e){
+            logger.error("No se pudo guardar el libro " + e.getMessage());
+        }
+
+        if (returnedLibro != null) {
+            return new ResponseEntity<>(returnedLibro, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(returnedLibro, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/api/nuevo/audio-libro")
+    public ResponseEntity<AudioLibro> crearLibro(@RequestBody AudioLibro audioLibro) {
+        AudioLibro returnedAudioLibro = null;
+        try {
+            returnedAudioLibro = audioLibroRepository.save(audioLibro);
+        } catch (Exception e){
+            logger.error("No se pudo guardar el audio-libro " + e.getMessage());
+        }
+
+        if (returnedAudioLibro != null) {
+            return new ResponseEntity<>(returnedAudioLibro, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(returnedAudioLibro, HttpStatus.BAD_REQUEST);
+        }
     }
 }
